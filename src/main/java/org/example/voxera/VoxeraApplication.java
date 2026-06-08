899d78f60d79a837a0a285cc42f0fbed69d20b60
Samvel-opta-data <your-email@example.com>
2026-06-08 09:38:50 +0200

    Initial commit: Voxera project

---
+++ src/main/java/org/example/voxera/VoxeraApplication.java
@@ -0,0 +1,13 @@
+package org.example.voxera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxeraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoxeraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/voxera/VoxeraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.voxera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoxeraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
