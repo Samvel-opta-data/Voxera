30eb3ba1423cc10a9cedfb763b3b019015fb7bc3
Sheydey <hatafata@gmail.com>
2026-06-09 18:43:48 +0200

    Homepage.html: First commit. PageController.java: Switched RestController to Controller. pom.xml: JavaVersion 25 -> 21, SpringBoot Version 4.0.6 -> 3.5.6, added thymeleaf dependency for .html file usage.

--- src/main/java/voxera/controller/PageController.java
+++ src/main/java/voxera/controller/PageController.java
@@ -4,16 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PageController {
 
     @GetMapping("/")
     public String home() {
-        return "<h1> Willkommen auf Voxera!</h1>";
-    }
-
-    @GetMapping("/impressum")
-    public String impressum() {
-        return "<h1> Samvel, Dustin, Vladyslav</h1>";
+        return "Homepage";
     }
 }
