bf2e4a8886a02f9e0fbebf0a4c23f17b6c655cde
Samvel-opta-data <your-email@example.com>
2026-06-09 14:38:51 +0200

    Lombok

--- src/main/java/voxera/entity/message.java
+++ src/main/java/voxera/entity/message.java
@@ -1,40 +1,17 @@
 package voxera.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class message {
+
     private String content;
     private String sender;
     private String timestamp;
-
-    public message() {
-    }
-
-    public message(String content, String sender, String timestamp) {
-        this.content = content;
-        this.sender = sender;
-        this.timestamp = timestamp;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
 }
--- src/main/java/voxera/entity/user.java
+++ src/main/java/voxera/entity/user.java
@@ -1,30 +1,17 @@
 package voxera.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class user {
 
     private String username;
     private String password;
-
-    public user() {
-    }
-
-    public user(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }}
+}
